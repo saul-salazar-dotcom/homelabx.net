7e79c1d2f0281c7953e79b397e3389e447a0e758
Saul Salazar <saulmmendoza+github@gmail.com>
2024-10-26 20:26:36 -0600

    feat: add env var RECREATE to force docker recreate

--- start.sh
+++ start.sh
@@ -4,6 +4,12 @@ path=$(pwd)
 for name in authelia proxy smtp tv downloads torrents notes changelog wiki whiteboard design read schedule plan 
 do
     echo "🚀 Starting service ${name}"
-    cd "$path/$name" && docker compose up -d --force-recreate
+    cd "$path/$name"
+    if [ -z "$RECREATE" ]; then
+        docker compose up -d
+    else
+        docker compose up -d --force-recreate
+    fi
 done
+
 echo "✅ All Services started"
