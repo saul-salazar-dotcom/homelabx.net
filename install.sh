59c86a9921cdb4b31543f60c9a1aec4594093fa4
Saul Salazar <saulmmendoza+github@gmail.com>
2024-10-26 20:20:35 -0600

    fix: root CA path

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ git clone https://github.com/saul-salazar-dotcom/homelabx.net.git ~/homelabx
 
 cd ~/homelabx/proxy && docker compose up -d
 
-FILE="$HOME/homelabx/proxy/rootCA.pem"
+FILE="$HOME/homelabx/proxy/certs/rootCA.pem"
 echo "⏳ Waiting for mkcert to create a root CA..."
 while true; do
     if [[ -f "$FILE" ]]; then
-        ln -s proxy/rootCA.pem plan/rootCA.pem
-        ln -s proxy/rootCA.pem design/rootCA.pem
+        ln -s "$FILE" plan/rootCA.pem
+        ln -s "$FILE" design/rootCA.pem
         break
     fi
     sleep 5  # Wait for 5 seconds before checking again
